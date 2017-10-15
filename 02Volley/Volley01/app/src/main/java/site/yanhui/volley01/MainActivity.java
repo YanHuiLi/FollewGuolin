fa33373725016c2f26f6a95996193a3a1a585566
Archer <421228286@qq.com>
2017-10-15 14:55:06 +0800

    完成StringRequest

---
+++ 02Volley/Volley01/app/src/main/java/site/yanhui/volley01/MainActivity.java
@@ -0,0 +1,48 @@
+package site.yanhui.volley01;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //发送一个string请求
+        StringRequest();
+        
+
+
+    }
+
+    private void StringRequest() {
+        //1.创建一个队列
+        RequestQueue mQueue = Volley.newRequestQueue(MainActivity.this);
+
+        //2.创建一个请求
+        StringRequest stringRequest = new StringRequest("http://www.yanhui.site",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        //打印返回值
+                        Log.d("TAG", response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //打印错误
+                Log.e("TAG", error.getMessage(), error);
+            }
+        });
+        //3.添加进队列
+        mQueue.add(stringRequest);
+    }
+}
