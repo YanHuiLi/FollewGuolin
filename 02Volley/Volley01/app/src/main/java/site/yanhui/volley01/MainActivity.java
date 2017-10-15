82b9e1eb2733807b068a3b9b9ad6bfe63f306779
Archer <421228286@qq.com>
2017-10-15 15:11:32 +0800

    fix some bug

--- 02Volley/Volley01/app/src/main/java/site/yanhui/volley01/MainActivity.java
+++ 02Volley/Volley01/app/src/main/java/site/yanhui/volley01/MainActivity.java
@@ -7,8 +7,20 @@ import android.util.Log;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+/**
+*create at 2017/10/15 by 15:10
+*作者：Archer
+*功能描述： 使用volley
+ * http://blog.csdn.net/guolin_blog/article/details/17482095
+ * stringRequest 字符串请求
+ * jsonRequest    json请求
+ *
+*/
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,10 +29,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //发送一个string请求
-        StringRequest();
-        
+//        StringRequest();
 
+        JsonRequest();
+    }
 
+    private void JsonRequest() {
+        RequestQueue mQueue = Volley.newRequestQueue(MainActivity.this);
+
+        //返回了一个json数据
+        String Url = "https://api.thinkpage.cn/v3/weather/now.json?key=rot2enzrehaztkdk&location=guangzhou&language=zh-Hans&unit=c";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("Tag", "onResponse: " + response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                 //打印错误
+                Log.e("TAG", error.getMessage(), error);
+            }
+        });
+
+        mQueue.add(jsonObjectRequest);
     }
 
     private void StringRequest() {
